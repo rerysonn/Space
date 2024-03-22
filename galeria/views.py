c9cafcd1652abab7aca0b0321a38fa1d2a99cad7
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-22 17:17:02 -0300

    Adicionando o index.html

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Olá Mundo</h1>')
+    return render(request, 'index.html')
